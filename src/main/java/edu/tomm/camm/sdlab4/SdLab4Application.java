d0b523591c5372eec8294d6a292aff2607cd7e0a
Tommaso Cammelli <tomm.camm@protonmail.ch>
2021-05-19 15:44:47 +0200

    initial population

---
+++ src/main/java/edu/tomm/camm/sdlab4/SdLab4Application.java
@@ -0,0 +1,13 @@
+package edu.tomm.camm.sdlab4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdLab4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdLab4Application.class, args);
+    }
+
+}
---
+++ src/test/java/edu/tomm/camm/sdlab4/SdLab4ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tomm.camm.sdlab4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdLab4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
